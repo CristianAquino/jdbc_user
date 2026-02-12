2b3ff7b7273b9e1f93a2ed7aa385e1d323020ab2
cristianaquino <ycristian10@gmail.com>
2026-02-11 22:23:37 -0500

    refactor: add exception to database error

--- src/main/java/com/jdbc/jdbc_user/Service/Impl/UserServiceImpl.java
+++ src/main/java/com/jdbc/jdbc_user/Service/Impl/UserServiceImpl.java
@@ -1,14 +1,19 @@
 package com.jdbc.jdbc_user.Service.Impl;
 
+import com.jdbc.jdbc_user.Exception.BusinessException;
 import com.jdbc.jdbc_user.Model.Dto.UserDto;
+import com.jdbc.jdbc_user.Model.Request.UserRequest;
 import com.jdbc.jdbc_user.Model.UserModel;
 import com.jdbc.jdbc_user.Repository.UserRepository;
 import com.jdbc.jdbc_user.Service.UserService;
+import com.jdbc.jdbc_user.Utils.CleanErroResponse;
+import com.jdbc.jdbc_user.Utils.TransformDataUserDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
+import java.sql.SQLException;
 import java.util.List;
 
 @Service
@@ -16,58 +21,82 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private CleanErroResponse cleanErroResponse;
+    private TransformDataUserDto transformDataUserDto;
 
     @Override
-    public List<UserModel> allUsers() {
-        List<UserModel> users = userRepository.getAllUsers();
-        return users;
+    public List<UserModel> allUsers(Integer is_soft) {
+        try{
+            List<UserModel> users = userRepository.getAllUsers(is_soft);
+            return users;
+        }catch (DataAccessException e){
+            SQLException sqlEx = (SQLException) e.getCause();
+            throw new BusinessException(
+                    sqlEx.getErrorCode(),
+                    cleanErroResponse.transform(sqlEx.getMessage())
+            );
+        }
+
     }
 
-    public List<UserDto> allUsersDto(){
-        List<UserModel> users = userRepository.getAllUsers();
-        List<UserDto> usersDto = transformListDataDto(users);
-        return usersDto;
+    public List<UserDto> allUsersDto(Integer is_soft){
+        try{
+            List<UserModel> users = userRepository.getAllUsers(is_soft);
+            List<UserDto> usersDto = transformDataUserDto.transformListDataDto(users);
+            return usersDto;
+        }catch (DataAccessException e){
+            SQLException sqlEx = (SQLException) e.getCause();
+            throw new BusinessException(
+                    sqlEx.getErrorCode(),
+                    cleanErroResponse.transform(sqlEx.getMessage())
+            );
+        }
     }
 
     @Override
     public UserDto oneUserById(Integer id) {
-        UserModel user = userRepository.getOneUserById(id);
-        UserDto userDto = transformOneDataDto(user);
-        return userDto;
+        try{
+            UserModel user = userRepository.getOneUserById(id);
+            UserDto userDto = transformDataUserDto.transformOneDataDto(user);
+            return userDto;
+        }catch (DataAccessException e){
+            SQLException sqlEx = (SQLException) e.getCause();
+            throw new BusinessException(
+                    sqlEx.getErrorCode(),
+                    cleanErroResponse.transform(sqlEx.getMessage())
+            );
+        }
     }
 
     @Transactional
     @Override
-    public UserDto createUser(UserModel user) {
-        UserModel register = userRepository.postUserRegister(user);
-        UserDto dto = transformOneDataDto(register);
-        return dto;
+    public UserDto createUser(UserRequest user) {
+        try{
+            UserModel register = userRepository.postUserRegister(user);
+            UserDto dto = transformDataUserDto.transformOneDataDto(register);
+            return dto;
+        }catch (DataAccessException e){
+            SQLException sqlEx = (SQLException) e.getCause();
+            throw new BusinessException(
+                    sqlEx.getErrorCode(),
+                    cleanErroResponse.transform(sqlEx.getMessage())
+            );
+        }
     }
 
     @Transactional
     @Override
     public UserDto delUserById(Integer id) {
-        UserModel del = userRepository.delUserById(id);
-        UserDto dto = transformOneDataDto(del);
-        return dto;
-    }
-
-    public static List<UserDto> transformListDataDto(List<UserModel> list){
-        List<UserDto> dto = new ArrayList<>();
-        for (UserModel user : list){
-            dto.add(new UserDto(user.getUsername(),
-                    user.getFirstname(),
-                    user.getLastname(),
-                    user.getCountry()));
+        try{
+            UserModel del = userRepository.delUserById(id);
+            UserDto dto = transformDataUserDto.transformOneDataDto(del);
+            return dto;
+        }catch(DataAccessException e){
+            SQLException sqlEx = (SQLException) e.getCause();
+            throw new BusinessException(
+                    sqlEx.getErrorCode(),
+                    cleanErroResponse.transform(sqlEx.getMessage())
+            );
         }
-        return dto;
-    }
-
-    public static UserDto transformOneDataDto(UserModel user){
-        UserDto dto = new UserDto(user.getUsername(),
-                    user.getFirstname(),
-                    user.getLastname(),
-                    user.getCountry());
-        return dto;
     }
 }
