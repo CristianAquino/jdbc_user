11af2dc1b1bcc896bf697fed804f5822e692a8bc
cristianaquino <ycristian10@gmail.com>
2026-02-11 22:16:06 -0500

    refactor: add api response and validation

--- src/main/java/com/jdbc/jdbc_user/Controller/UserController.java
+++ src/main/java/com/jdbc/jdbc_user/Controller/UserController.java
@@ -1,42 +1,99 @@
 package com.jdbc.jdbc_user.Controller;
 
 import com.jdbc.jdbc_user.Model.Dto.UserDto;
+import com.jdbc.jdbc_user.Model.Request.UserRequest;
+import com.jdbc.jdbc_user.Model.Response.ApiResponse;
 import com.jdbc.jdbc_user.Model.UserModel;
 import com.jdbc.jdbc_user.Service.UserService;
+import com.jdbc.jdbc_user.Utils.ResponseBuilder;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Validated
 public class UserController {
     private final UserService userService;
+    private ResponseBuilder responseBuilder;
 
     @GetMapping("/all")
-    public ResponseEntity<List<UserModel>> allUsers(){
-        List<UserModel> gretting = userService.allUsers();
-        return ResponseEntity.ok(gretting);
+    public ResponseEntity<ApiResponse<List<UserModel>>> allUsers(
+            @RequestParam(required = false)  @Min(0) @Max(1) Integer is_soft,
+            HttpServletRequest http){
+            List<UserModel> datos = userService.allUsers(is_soft);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(responseBuilder
+                        .response(
+                                datos,
+                                HttpStatus.OK,
+                                http.getRequestURI()
+                        )
+                );
     }
 
     @GetMapping("/all/dto")
-    public ResponseEntity<List<UserDto>> allUsersDto(){
-        List<UserDto> gretting = userService.allUsersDto();
-        return ResponseEntity.ok(gretting);
+    public ResponseEntity<ApiResponse<List<UserDto>>> allUsersDto(
+            @RequestParam(required = false)Integer is_soft,
+            HttpServletRequest http){
+        List<UserDto> datos = userService.allUsersDto(is_soft);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(responseBuilder
+                        .response(
+                                datos,
+                                HttpStatus.OK,
+                                http.getRequestURI()
+                        )
+                );
     }
 
     @GetMapping("/one/{id}")
-    public ResponseEntity<UserDto> oneUserById(@PathVariable Integer id){
-        UserDto response = userService.oneUserById(id);
-        return ResponseEntity.status(HttpStatus.OK).body(response);
+    public ResponseEntity<ApiResponse<UserDto>> oneUserById(
+            @PathVariable @Valid @Positive Integer id,
+            HttpServletRequest http){
+        UserDto datos = userService.oneUserById(id);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(responseBuilder
+                        .response(
+                                datos,
+                                HttpStatus.OK,
+                                http.getRequestURI()
+                        ));
     }
 
     @PostMapping("/register")
-    public ResponseEntity<UserDto> oneUserRegister(@RequestBody UserModel user){
-        UserDto register = userService.createUser(user);
-        return ResponseEntity.status(HttpStatus.CREATED).body(register);
+    public ResponseEntity<ApiResponse<UserDto>> oneUserRegister(
+            @RequestBody @Valid UserRequest user,
+            HttpServletRequest http){
+        UserDto data = userService.createUser(user);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentContextPath()
+                .path("/one/{id}")
+                .buildAndExpand(data.getId())
+                .toUri();
+        return ResponseEntity
+                .created(location)
+                .body(responseBuilder
+                        .response(
+                                data,
+                                HttpStatus.CREATED,
+                                http.getRequestURI()
+                        )
+                );
     }
 
     @PutMapping("/upd/{id}")
@@ -45,8 +102,17 @@ public class UserController {
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<UserDto> oneDeleteById(@PathVariable Integer id){
-        UserDto del = userService.delUserById(id);
-        return ResponseEntity.status(HttpStatus.ACCEPTED).body(del);
+    public ResponseEntity<ApiResponse<UserDto>> oneDeleteById(
+            @PathVariable @Valid @Positive Integer id,
+            HttpServletRequest http){
+        UserDto data = userService.delUserById(id);
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(responseBuilder
+                        .response(
+                                data,
+                                HttpStatus.ACCEPTED,
+                                http.getRequestURI()
+                        ));
     }
 }
